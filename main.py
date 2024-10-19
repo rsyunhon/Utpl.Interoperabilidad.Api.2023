e36a210a01c518a2b7e38ea0a07602cd0eaf7033
Ricardo Yunhon <148500851+rsyunhon@users.noreply.github.com>
2024-10-19 02:14:37 +0000

    Info

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ database = cliente["Interoperatividad"]
 coleccion = database["apiPedido"]
 
 #libreria para el manejo de versiones
-from versioned_fastapi import version, FastApiVersioner
+#from versioned_fastapi import version, FastApiVersioner
 
 app = FastAPI(
     title="API de pedidos del segundo parcial",
@@ -59,7 +59,7 @@ class PedidoRepositorio(BaseModel):
 # pedido_db = []
 
 # Operación para crear un pedido
-@version(1)
+#@version(1)
 @app.post("/pedido/", response_model=PedidoRepositorio, tags=["Pedido"] )
 def create_pedido(pedido: Pedido):
     idPedido=str(uuid.uuid4())
@@ -69,7 +69,7 @@ def create_pedido(pedido: Pedido):
     return itemPedido
 
 # Operación para obtener todas los pedidos
-@version(1)
+#@version(1)
 @app.get("/pedido/", response_model=List[PedidoRepositorio], tags=["Pedido"])
 def get_all_pedido():
     items = list(coleccion.find())
@@ -77,7 +77,7 @@ def get_all_pedido():
     return pedidos
 
 # Operación para obtener un pedido por ID
-@version(1)
+#@version(1)
 @app.get("/pedido/{pedido_id}", response_model=PedidoRepositorio, tags=["Pedido"])
 def get_pedido_by_id(pedido_id: str):
     item = coleccion.find_one({"_id": ObjectId(pedido_id)})
@@ -87,7 +87,7 @@ def get_pedido_by_id(pedido_id: str):
         raise HTTPException(status_code=404, detail="Pedido no encontrado")
 
 # Operación para editar un pedido por ID
-@version(1)
+#@version(1)
 @app.put("/pedido/{pedido_id}", response_model=PedidoRepositorio, tags=["Pedido"])
 def update_pedido(pedido_id: str, updated_pedido: Pedido):
     result = coleccion.update_one(
@@ -100,7 +100,7 @@ def update_pedido(pedido_id: str, updated_pedido: Pedido):
         raise HTTPException(status_code=404, detail="Pedido no encontrado")
 
 # Operación para eliminar un pedido por ID
-@version(1)
+#@version(1)
 @app.delete("/pedido/{pedido_id}", response_model=PedidoRepositorio, tags=["Pedido"])
 def delete_pedido(pedido_id: str):
     item = coleccion.find_one_and_delete({"_id": ObjectId(pedido_id)})
@@ -110,7 +110,7 @@ def delete_pedido(pedido_id: str):
         raise HTTPException(status_code=404, detail="Pedido no encontrado")
 
 # Operación para obtener una pedido por identificacion
-@version(1)
+#@version(1)
 @app.get("/pedido/mesa/{mesa}", response_model=Pedido, tags=["Pedido"])
 def get_pedido_by_mesa(mesa: int):
     item = coleccion.find_one({"mesa": mesa})
@@ -122,4 +122,4 @@ def get_pedido_by_mesa(mesa: int):
 
 # Version your app
 # It will add version prefixes and customize the swagger docs
-versions = FastApiVersioner(app).version_fastapi()
+#versions = FastApiVersioner(app).version_fastapi()
